90f61090d66b2543f7ad511ed2ce56d43e458e63
HuyALT <huytn102030z@gmail.com>
2025-07-10 20:16:19 +0700

    NewPost
    DangBai

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import MainLayout from './components/layout/MainLayout';
-import HomePage from './features/home/HomePage';
-import AboutPage from './features/about/AboutPage';
-import ContactPage from './features/contact/ContactPage';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainLayout from "./components/layout/MainLayout";
+import HomePage from "./features/home/HomePage";
+import AboutPage from "./features/about/AboutPage";
+import ContactPage from "./features/contact/ContactPage";
 
-import './App.css';
-import Login from './features/auth/login';
-import SignUp from './features/auth/SignUp';
+import "./App.css";
+import Login from "./features/auth/login";
+import SignUp from "./features/auth/SignUp";
 
 function App() {
   return (
@@ -17,7 +17,7 @@ function App() {
           <Route path="/about" element={<AboutPage />} />
           <Route path="/contact" element={<ContactPage />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp/>}/>
+          <Route path="/signup" element={<SignUp />} />
         </Routes>
       </MainLayout>
     </Router>
